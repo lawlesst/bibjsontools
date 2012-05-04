d4f4deb17c30d41023306bdcf054b9d0216cb5ef
lawlesst <lawlesst@gmail.com>
2012-05-04 10:59:14 -0300

    Fixed package name.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bibjsonutils',
+    name='bibjsontools',
     version='0.1',
     author='Ted Lawless',
     author_email='lawlesst@gmail.com',
     packages=find_packages(),
-    package_data={'bibjsonutils': ['test/data/*.*']},
+    package_data={'bibjsontools': ['test/data/*.*']},
 )
 
 
