481a879df9bab78d95e1fc7d25bf0ade53848b7a
Ted Lawless <lawlesst@gmail.com>
2012-05-16 10:56:04 -0700

    More tests.

--- bibjsontools/openurl.py
+++ bibjsontools/openurl.py
@@ -38,10 +38,355 @@ def pull_and_map(key_list, cite):
         val = cite.get(k, None)
         if val:
             return val[0].strip()
-    return 
+    return
 
 
+class Common(object):
+    pass
+
+
+import urlparse
+class OpenURLParser(object):
+
+    def __init__(self, openurl):
+        self.query = openurl
+        self.data = urlparse.parse_qs(openurl)
+
+    def _find_key(self, key_list, this_dict=None):
+        """
+        Utility to get the first matching value from a list of possible keys.
+        """
+        #Default to the data dict.
+        if not this_dict:
+            this_dict = self.data
+        for k in key_list:
+            v = this_dict.get(k, None)
+            if v:
+                return v[0]
+        return
+
+    def _find_repeating_key(self, key_list, this_dict=None):
+        """
+        Utility to get a unique list of values from a set of keys.
+        """
+        out = []
+        #Default to the data dict.
+        if not this_dict:
+            this_dict = self.data
+        for k in key_list:
+            v = this_dict.get(k, None)
+            if v:
+                out += v
+        return set(out)
+
+    def _find_key_values(self, key_list, this_dict=None):
+        """
+        Utility to return a list of key,value tuples from a list of possible keys.
+        """
+        #Default to the data dict.
+        if not this_dict:
+            this_dict = self.data
+        out = []
+        for k in key_list:
+            v = this_dict.get(k, None)
+            if v:
+                out.append((k, v))
+        return out
+
+    @property
+    def type(self):
+        """
+        Determine the type of citation.
+        """
+        btype = 'unknown'
+        #Try the format first
+        fmt = self.data.get('rft_val_fmt', None)
+        #if fmt:
+            #if fmt =='info:ofi/fmt:kev:mtx:journal':
+            #    return 'article'
+            #elif fmt == 'info:/ofi:
+        genre = self._find_key(['rft.genre', 'genre'])
+        if genre:
+            if genre == 'bookitem':
+                btype = 'inbook'
+            else:
+                btype = genre
+        else:
+            #Try to guess based on incoming values.
+            if self._find_key(['rft.atitle', 'atitle']):
+                btype = 'article'
+            elif self._find_key(['rft.btitle', 'btitle']):
+                btype = 'book'
+        return btype
+
+    def identifiers(self):
+        """
+        Pull the identifiers.  This should be common to all types.
+        """
+        out = []
+        #Identifiers - using both the standard and what's found in typical OpenURLs
+        ids = self._find_key_values(['rft.id',
+                                     'rft_id',
+                                     'id',
+                                     'doi',
+                                     'pmid',
+                                     'pid',
+                                     'rfe_dat'])
+        for k, values in ids:
+            for v in values:
+                d = {}
+                d['id'] = None
+                d['type'] = None
+                #DOIS and PMIDS in the id
+                if (k == 'rft.id') or (k == 'rft_id') or (k == 'id'):
+                    if v.startswith('info:doi/'):
+                        #referent['doi'] = v.lstrip('info:doi/')
+                        d['type'] = 'doi'
+                        d['id'] = "doi:%s" % v.lstrip('info:doi/')
+                    elif v.startswith('info:pmid/'):
+                        d['type'] = 'pmid'
+                        d['id'] = v
+                #OCLC
+                elif (k == 'rfe_dat') and ('accessionnumber' in v):
+                    d['type'] = 'oclc'
+                    d['id'] = v.replace('<accessionnumber>', '').replace('</accessionnumber>', '')
+                #Other ids from the wild
+                elif k == 'pmid':
+                    id['type'] = 'pmid'
+                    id['id'] = v
+                elif k == 'doi':
+                    id['type'] = 'doi'
+                    id['id'] = "doi:%s" % v
+                #If we found an id add it to the output.
+                if d['id']:
+                    out.append(d)
+        #ISBNS and ISSNs are more straightforward so will handle them separately.
+        for isbn in self._find_repeating_key(['rft.isbn', 'isbn']):
+            out.append({'type': 'isbn',
+                        'id': isbn})
+        for issn in self._find_repeating_key(['rft.issn', 'issn']):
+            out.append({'type': 'issn',
+                        'id': issn})
+        for eissn in self._find_repeating_key(['rft.eissn', 'eissn']):
+            out.append({'type': 'eissn',
+                        'id': issn})
+        return out
+
+    def titles(self):
+        out = {}
+        #Article or book titles will be set to bibjson title.
+        out['title'] = self._find_key(['rft.atitle',
+                                       'atitle',
+                                       'rft.btitle',
+                                       'btitle'])
+        #Journal title
+        if self.type in ['article', 'inbook']:
+            jtitle = self._find_key(['rft.jtitle',
+                                     'jtitle',
+                                     'rft.title',
+                                     'title'])
+            if jtitle:
+                ti = {'name': jtitle}
+                #Try to pull short title code.
+                stitle = self.data.get('rft.stitle', None)
+                if not stitle:
+                    stitle = self.data.get('stitle', None) 
+                if stitle:
+                    ti['shortcode'] = stitle[0] 
+                out['journal'] = ti
+        return out
+
+    def authors(self):
+        """
+        Pull authors.  Less straightforward than you might think.  
+        For now, we won't worry about first and last names.
+        """
+        out = []
+        authors = self._find_key_values([
+                                        'rft.au',
+                                        'au',
+                                        'rft.aulast',
+                                        'aulast'])
+        for k,values in authors:
+            for v in values:
+                if (k == 'rft.au') or (k == 'au') or\
+                    (k == 'rft.aulast') or (k == 'aulast'):
+                    #If it's a full name, set here.  
+                    if (k == 'rft.au') or (k == 'au'): 
+                        au = {'name': v}
+                    else:
+                        au = {}
+                    aulast = self._find_key(['rft.aulast', 'aulast'])
+                    if aulast:
+                        au['lastname'] = aulast
+                    aufirst = self._find_key(['rft.aufirst', 'aufirst'])
+                    if aufirst:
+                        au['firstname'] = aufirst
+                    
+                    #Put the full name together now if we can. 
+                    if not au.has_key('name'):
+                        #If there isn't a first and last name, just use last.
+                        last = au.get('lastname', '')
+                        first = au.get('firstname', '')
+                        name = "%s, %s" % (last, first.strip())
+                        au['name'] = name.rstrip(', ')
+                    out.append(au)
+        return out
+
+    def pages(self):
+        """
+        Try to set start, end page and pages.
+        """
+        out = {}
+        #Pages
+        out['pages'] = self._find_key(['rft.pages', 'pages'])
+        start = self._find_key(['rft.spage', 'spage'])
+        end = self._find_key(['rft.epage', 'epage'])
+        if (not out['pages']):
+            if start:
+                #Default end_page is EOA - end of article
+                if not end:
+                    end = 'EOA'
+                #Default end page to end of article
+                pages = "%s - %s" % (start, end)
+                out['pages'] = pages.strip()
+        
+        out['end_page'] = end
+        out['start_page'] = start
+
+        return out
+
+
+    def parse(self):
+        """
+        Create and return the bibjson.
+        """
+        #from pprint import pprint
+        #pprint(self.data)
+        d = {}
+        d['type'] = self.type
+        #Referrer
+        d['_rfr'] = self._find_key(['rfr_id', 'sid'])
+        d['identifier'] = self.identifiers()
+        d.update(self.titles())
+        d['author'] = self.authors()
+        #Publisher
+        d['publisher'] = self._find_key(['rft.pub', 'pub'])
+        #Volume
+        d['volume'] = self._find_key(['rft.volume', 'volume'])
+        #Issue
+        d['issue'] = self._find_key(['rft.issue', 'issue'])
+        #Date/Year
+        year = self._find_key(['rft.date', 'date'])
+        if year:
+            d['year'] = year[:4]
+        #Pages
+        d.update(self.pages())
+        #Remove empty keys
+        for k,v in d.items():
+            if not v:
+                del d[k]
+        #add the original openurl
+        d['_openurl'] = self.query
+        return d
+        
 def from_openurl(query):
+    """
+    Alias/shortcut to parse the provided query.
+    """
+    b = OpenURLParser(query)
+    return b.parse()
+
+
+class BibJSONToOpenURL(object):
+    def __init__(self, bibjson):
+        self.data = bibjson
+
+    def parse(self):
+        #return self.data
+        """
+        Convert bibjson to an OpenURL.
+        start_page => 361
+        bul:rfr => FirstSearch:MEDLINE
+        title => The missing technology: an international comparison of human capital investment in healthcare.
+        type => article
+        journal => {'name': 'Applied health economics and health policy'}
+        author => [{'lastname': 'Frogner', 'name': 'BK Frogner', 'firstname': 'BK'}]
+        volume => 8
+        year => 2010
+        identifier => [{'type': 'issn', 'id': '1175-5652'}, {'type': 'oclc', 'id': '678061209'}]
+        issue => 6
+        pages => 361--71
+        end_page => 71
+        """
+        bib = self.data
+        out = {}
+        out['ctx_ver'] = 'Z39.88-2004'
+        btype = bib['type']
+        #By default we will treat unknowns as articles for now.
+        if (btype == 'article') or (btype=='unknown'):
+            if btype == 'article':
+                out['rft_val_fmt'] = 'info:ofi/fmt:kev:mtx:journal'
+                out['rft.atitle'] = bib['title']
+                jrnl = bib.get('journal', {})
+                out['rft.title'] = jrnl.get('name')
+                out['rft.jtitle'] = out['rft.title']
+                out['rft.stitle'] = jrnl.get('shortcode')
+        else:
+            out['rft_val_fmt'] = 'info:ofi/fmt:kev:mtx:book'
+            out['rft.btitle'] = bib['title']
+            if bib['type'] == 'inbook':
+                out['rft.genre'] = 'bookitem'
+
+        out['rfr_id'] = "info:sid/%s" % (bib.get('bul:rfr', ''))
+
+        #Do the common attributes
+        out['rft.date'] = bib.get('year')
+        authors = bib.get('author', [])
+        for auth in authors:
+            full = auth.get('name')
+            last = auth.get('lastname')
+            if full:
+                out['rft.au'] = full
+            elif last:
+                out['rft.aulast'] = last
+        out['rft.volume'] = bib.get('volume')
+        out['rft.issue'] = bib.get('issue')
+        out['rft.spage'] = bib.get('start_page')
+        out['rft.end_page'] = bib.get('end_page')
+        out['rft.pages'] = bib.get('pages')
+        identifiers = bib.get('identifier', [])
+        for idt in identifiers:
+            if idt['type'] == 'issn':
+                out['rft.issn'] = idt['id']
+            elif idt['type'] == 'isbn':
+                out['rft.isbn'] = idt['id']
+            elif idt['type'] == 'eissn':
+                out['rfit.eissn'] = idt['id']
+            elif idt['type'] == 'doi':
+                out['rft_id'] = 'info:doi/%s' % idt['id']
+            elif idt['type'] == 'pmid':
+                #don't add the info:pmid if not necessary
+                v = idt['id']
+                if v.startswith('info:pmid'):
+                    out['rft_id'] = v
+                else:
+                    out['rft_id'] = 'info:pmid/%s' % idt['id']
+            elif idt['type'] == 'oclc':
+                out['rft_id'] = 'http://www.worldcat.org/oclc/%s' % idt['id']
+        #Remove empty keys
+        for k,v in out.items():
+            if not v:
+                del out[k]
+        #from pprint import pprint
+        #pprint(out)
+        return urllib.urlencode(out)
+
+
+
+
+
+def old_from_openurl(query):
     """
     An attempt to map raw OpenURLs to BibJSON convention.  
      
@@ -58,22 +403,22 @@ def from_openurl(query):
     referent['identifier'] = []
     type = None
 
-    #Deterimine a citation type first - 
-    if cite.get('rft.atitle') or cite.get('atitle'):
-        type = 'article'
-    elif cite.get('rft.btitle') or cite.get('btitle'):
-        type = 'book'
+    genre = cite.get('rft.genre', None)
+    if genre:
+        type = genre[0]
     else:
-        type = 'unknown'
-
+        #Try to gues based on incoming values.
+        if cite.get('rft.atitle') or cite.get('atitle'):
+            type = 'article'
+        elif cite.get('rft.btitle') or cite.get('btitle'):
+            type = 'book'
+        else:
+            type = 'unknown'
 
     #Initialize an identifier
     id = initialize_id()
     for k,value_list in cite.items():
         for v in value_list:
-            #Get type
-            if k == 'genre':
-                type = v
 #            elif k == 'rft_val_fmt':
 #                if type is None:
 #                    if v == 'info:ofi/fmt:kev:mtx:journal':
@@ -81,7 +426,7 @@ def from_openurl(query):
 #                    elif v == 'info:ofi/fmt:kev:mtx:book':
 #                        type = 'book'
             #Article title
-            elif(k == 'rft.atitle') or (k == 'atitle'):
+            if(k == 'rft.atitle') or (k == 'atitle'):
                 referent['title'] = v
             #Book title
             elif k == ('rft.btitle') or (k == 'btitle'):
@@ -212,7 +557,12 @@ def from_openurl(query):
     referent['_openurl'] = to_openurl(referent)
     return referent
 
-def to_openurl(bib): 
+
+def to_openurl(bib):
+    out = BibJSONToOpenURL(bib)
+    return out.parse()
+
+def old_to_openurl(bib): 
     """
     Convert bibjson to an OpenURL.
     start_page => 361
@@ -230,11 +580,19 @@ def to_openurl(bib):
     """
     out = {}
     out['ctx_ver'] = 'Z39.88-2004'
-    if bib['type'] == 'article':
-        out['rft_val_fmt'] = 'info:ofi/fmt:kev:mtx:journal'
+    btype = bib['type']
+    if (btype == 'article') or (btype == 'bookitem'):
+        if btype == 'article':
+            out['rft_val_fmt'] = 'info:ofi/fmt:kev:mtx:journal'
+            out['rft.atitle'] = bib['title']
+        else:
+            out['rft_val_fmt'] = 'info:ofi/fmt:kev:mtx:book'
+            out['rft.genre'] = 'bookitem'
+            out['rft.title'] = bib['title']
+
         out['rfr_id'] = "info:sid/%s" % (bib.get('bul:rfr', ''))
-        out['rft.atitle'] = bib['title']
-        jrnl = bib.get('journal')
+        
+        jrnl = bib.get('journal', {})
         out['rft.title'] = jrnl.get('name')
         out['rft.jtitle'] = out['rft.title']
         out['rft.stitle'] = jrnl.get('shortcode')
@@ -263,6 +621,8 @@ def to_openurl(bib):
         for idt in identifiers:
             if idt['type'] == 'issn':
                 out['rft.issn'] = idt['id']
+            elif idt['type'] == 'isbn':
+                out['rft.isbn'] = idt['id']
             elif idt['type'] == 'eissn':
                 out['rfit.eissn'] = idt['id']
             elif idt['type'] == 'doi':
